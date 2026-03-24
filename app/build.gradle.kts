7dda81d4a1fa0d09594bc1f162822e0e8da3e118
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-03-24 10:06:41 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,2 +1,3 @@
 implementation("com.github.bumptech.glide:glide:4.16.0")
 annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+implementation("com.google.android.gms:play-services-ads:23.0.0")
