dc1fce6d9a82a63c2dcb17569e8d399e479442ed
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-03-25 12:41:47 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,33 @@
-implementation("com.github.bumptech.glide:glide:4.16.0")
-annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
-implementation("com.google.android.gms:play-services-ads:23.0.0")
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.gustavo.chefvisionia"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.gustavo.chefvisionia"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
+}
