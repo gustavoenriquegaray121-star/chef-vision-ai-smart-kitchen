e3d2040dd40ba754a332a60b2386dbb4b001b26a
gustavoenriquegaray121-star <gustavoenriquegaray121@gmail.com>
2026-03-26 19:29:52 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        // API Key segura desde GitHub Secrets
         buildConfigField(
             "String",
             "GEMINI_API_KEY",
@@ -52,23 +51,16 @@ android {
 }
 
 dependencies {
-    // CORE
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    // LIFECYCLE para lifecycleScope
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-
-    // GLIDE
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
-
-    // ADMOB
     implementation("com.google.android.gms:play-services-ads:23.0.0")
-
-    // PRUEBAS
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
